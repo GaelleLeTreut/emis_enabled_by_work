b1d39314733a3cef44abd5319047329b4350df0b
Gaëlle Le Treut <33718900+GaelleLeTreut@users.noreply.github.com>
2021-01-28 16:53:07 +0100

    mean emission content by wage class

--- salaries_insee.py
+++ salaries_insee.py
@@ -28,6 +28,7 @@ else:
 
 
 #Importer base salaires15 INSEE (euro 2015)
+#https://www.insee.fr/fr/statistiques/3536754#dictionnaire
 full_insee_table = pd.read_csv(data_dir + os.sep + 'salaries15.csv',sep=',',index_col=0, low_memory=False)
 dict_TRNNETO_to_salary = {0 : stat.mean([0,200]),1 : stat.mean([200,500]),2 : stat.mean([500,1000]),3 : stat.mean([1000,1500]),4 : stat.mean([1500,2000]),5 : stat.mean([2000,3000]),6 : stat.mean([3000,4000]),7 : stat.mean([4000,6000]),8 : stat.mean([6000,8000]),9 : stat.mean([8000,10000]),10 : stat.mean([10000,12000]), 11 : stat.mean([12000,14000]),12 : stat.mean([14000,16000]), 13 : stat.mean([16000,18000]), 14 : stat.mean([18000,20000]), 15 : stat.mean([20000,22000]), 16 : stat.mean([22000,24000]), 17 : stat.mean([24000,26000]), 18 : stat.mean([26000,28000]),19 : stat.mean([28000,30000]),20 : stat.mean([30000,35000]), 21 : stat.mean([35000,40000]), 22 : stat.mean([40000,50000]),23 : 60000 }
 full_insee_table['salary_value']=full_insee_table['TRNNETO'].replace(dict_TRNNETO_to_salary)
@@ -95,6 +96,17 @@ def ratio_of_mass2(lower_bound, higher_bound, label_to_sum, label_of_bound, data
     high_mass = np.sum( data[data[label_of_bound]>higher_bound][label_to_sum])
     return high_mass / low_mass
 
+def mean_emission_content(table):
+   mean = np.sum(table['A38'].replace(dic_to_emission_content))/len(table)
+   return mean
+
+# Mean emission by wage class
+mean_emis_content_by_class =pd.DataFrame(np.sort(full_insee_table['TRNNETO'].unique()),index=['class'+str(x) for x in list(np.sort(full_insee_table['TRNNETO'].unique()))])
+mean_emis_content_by_class.columns=['mean emission content']
+for i in np.sort(full_insee_table['TRNNETO'].unique()):
+    mean_emis_content_by_class['mean emission content'][i] = mean_emission_content(full_insee_table[full_insee_table['TRNNETO']==i])
+mean_emis_content_by_class.loc['Global mean']  =mean_emission_content(full_insee_table)
+    
 #Statistiques Descriptives
 #base salaire
 mean_salary=round(stat.mean(full_insee_table['salary_value']))
@@ -122,3 +134,16 @@ if dispersion:
     print("Il y a plus d'inégalités d'émissions que de salaires")
 else:
     print("Il y a plus d'inégalités de salaires que d'émissions")
+    
+## PLOT
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_context('paper', font_scale=0.9)
+
+plt.figure(figsize=(18, 12))
+sns.barplot(x=mean_emis_content_by_class.index, y="mean emission content", data=mean_emis_content_by_class,palette='deep')
+plt.xlabel("wage class", size=12)
+plt.ylabel("gCO2/euro", size=12)
+plt.title("Mean emission content by wage classes", size=12)
+plt.savefig(OUTPUTS_PATH+'fig_mean_emis_cont_by_class.jpeg', bbox_inches='tight')
+plt.show()
