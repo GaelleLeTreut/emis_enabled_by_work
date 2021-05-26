53d7e46a85d21dab08bf9b4d04615e62cd3993d3
Gaëlle Le Treut <letreut@centre-cired.fr>
2021-05-27 01:35:30 +0200

    work in progress income_based analysis
    incoporating previous work (plots,tables, etc)
    TO CHECK...post and pre calculation
    influence of calc_all

--- income_based_test.py
+++ income_based_test.py
@@ -151,13 +151,19 @@ else:
 ##########################
 ###### AGGREGATION PRE CALCULATION
 ##########################
-# Correspondance au plus proche de A38 ( 35 sectors)
+## Aggregation into 35 sectors (closest correspondance with A38 nomenclature)
 # corresp_table = pd.read_csv(DATA_PATH + 'exiobase_A38.csv', comment='#',header=[0,1], index_col=0, sep=';')
 # corresp_table=np.transpose(corresp_table)
-
 # sector_agg = corresp_table.values
-# io_orig.aggregate(sector_agg=sector_agg,sector_names = list(corresp_table.index.get_level_values(0)))   
-    
+
+# ## Aggregation into two regions: FR and RoW
+# region_table = pd.read_csv(DATA_PATH + 'exiobase_FRvsRoW.csv', comment='#', index_col=0, sep=';')
+# region_table=np.transpose(region_table)
+# region_agg = region_table.values
+
+# io_orig.aggregate(region_agg=region_agg, sector_agg=sector_agg, region_names = list(region_table.index.get_level_values(0)), sector_names = list(corresp_table.index.get_level_values(0)))
+
+# region_list = list(io_orig.get_regions()) 
 
 ##########################
 ###### CALCULATION
@@ -168,14 +174,11 @@ io_orig.calc_system()
 io_orig.GHG_emissions.calc_system(x=io_orig.x, Y=io_orig.Y, L=io_orig.L, Y_agg=None, population=io_orig.population)
 io_orig.GHG_emissions.calc_income_based(x = io_orig.x, V=io_orig.V, G=io_orig.G, V_agg=None, population=io_orig.population)
 
-#io_orig.GHG_emissions.D_iba.sum(axis=0,level='region')
-#pour avoir les D_iba avec seulement le pays pour l'origine des émissions
-
 
 ##########################
 ###### AGGREGATION POST CALCULATION
 ##########################
-# Aggregation into 35 sectors (closest correspondance with A38 nomenclature)
+## Aggregation into 35 sectors (closest correspondance with A38 nomenclature)
 corresp_table = pd.read_csv(DATA_PATH + 'exiobase_A38.csv', comment='#',header=[0,1], index_col=0, sep=';')
 corresp_table=np.transpose(corresp_table)
 sector_agg = corresp_table.values
@@ -186,56 +189,167 @@ region_table=np.transpose(region_table)
 region_agg = region_table.values
 
 io_orig.aggregate(region_agg=region_agg, sector_agg=sector_agg, region_names = list(region_table.index.get_level_values(0)), sector_names = list(corresp_table.index.get_level_values(0)))
-## To fix : V is not aggregated
 
+region_list = list(io_orig.get_regions())
+
+##### GLT  TO check
 ## Do we really need to put here a calc all to reassess the variables that are not aggregated? A, B L and G are not calculted...
 io_orig.calc_all()
 
 
 ##########################
-###### Emission content
+###### Emission content and decomposition
 ##########################
-inc_emis_content = pymrio.calc_N(io_orig.GHG_emissions.S, io_orig.G)*1e-3
-
-
+#### 10¨3gco2/10¨6euro =  X gCO2/keuro => X * 1e-3 gCO2/e 
 ######
 ### Décomposition  Emission  content (in gCO2/euro)  = S + SB + SB^2 + ..
 ######
-inc_emis_content_direct = io_orig.GHG_emissions.S*1e-3
-inc_emis_content_fo = pymrio.calc_N(io_orig.GHG_emissions.S, io_orig.B)*1e-3
-inc_emis_content_so = pymrio.calc_N(io_orig.GHG_emissions.S, io_orig.B.dot(io_orig.B))*1e-3
+# inc_emis_content = pymrio.calc_N(io_orig.GHG_emissions.S, io_orig.G)*1e-3 = io_orig.GHG_emissions.N*1e-3
+inc_emis_cont = pd.DataFrame(np.transpose((io_orig.GHG_emissions.N*1e-3).sum(level=0,axis=1).sum(level=1,axis=0)).stack(),columns=['emission content'])
 
-## FRANCE only 
-# emission_content_FR = np.transpose(inc_emis_content).loc[['FR']]
-# income_based_emis_FR = np.transpose(io_orig.income_based_emis).loc[['FR']]
+inc_emis_content_direct = pd.DataFrame(np.transpose((io_orig.GHG_emissions.S*1e-3).sum(level=0,axis=1).sum(level=1,axis=0)).stack(),columns=['Direct emis content'])
+inc_emis_content_fo = pd.DataFrame(np.transpose((pymrio.calc_N(io_orig.GHG_emissions.S, io_orig.B)*1e-3).sum(level=0,axis=1).sum(level=1,axis=0)).stack(),columns=['FO emis content'])
+inc_emis_content_so =pd.DataFrame(np.transpose((pymrio.calc_N(io_orig.GHG_emissions.S, io_orig.B.dot(io_orig.B))*1e-3).sum(level=0,axis=1).sum(level=1,axis=0)).stack(),columns=['SO emis content'])
+
+inc_emis_cont_decomp = inc_emis_content_direct.copy()
+inc_emis_cont_decomp.loc[:,'FO emis content'] = inc_emis_content_fo['FO emis content']
+inc_emis_cont_decomp.loc[:,'SO emis content'] = inc_emis_content_so['SO emis content']
+inc_emis_cont_decomp.loc[:,'Rest emis content'] =inc_emis_cont['emission content'] - ( inc_emis_content_direct['Direct emis content'] + inc_emis_content_fo['FO emis content'] + inc_emis_content_so['SO emis content']) 
+
+
+## income based emission in France in MtCO2 
+income_based_emis_FR =io_orig.GHG_emissions.D_iba.sum(axis=1).loc[['FR']]*1e-9
+#io_orig.GHG_emissions.D_iba.sum(axis=0,level='region')
+#pour avoir les D_iba avec seulement le pays pour l'origine des émissions
+
+##########################
+###### PLOTS 
+##########################
+sns.set_context('paper', font_scale=0.9)
 
 ######
-### Decomposition enabled emissions (in gCO2/euro)
+### Emission content - Histogramme groupé FR vs ROW
 ######
-# emis_enable = inc_emis_content.copy()
-# for r in region_list:
-#     rest_list = list(region_list)
-#     rest_list.remove(r)
-    
-#     S_enable = io_orig.GHG_emissions.S*1e-3
-#     S_enable.loc[rest_list]=0
-#     emis_enable_r = pymrio.calc_iec(S_enable, io_orig.G)
-#     emis_enable_r.rename(index={'emission content':'emis content from '+r},inplace=True)
-#     emis_enable = emis_enable.append(emis_enable_r)
-#     del rest_list
-# emis_enable = emis_enable.drop(['emission content'])
+check=inc_emis_cont.reset_index(inplace=True)
+
+plt.figure(figsize=(18, 12))
+sns.barplot(x="sector", hue="region", y="emission content", data=inc_emis_cont)
+plt.xlabel("Sector code", size=12)
+plt.ylabel("g$\mathrm{CO}_2$eq/\euro", size=12)
+plt.title("Emission content - France vs Rest of World", size=12)
+plt.savefig(OUTPUTS_PATH+'fig_emis_cont_FRvsRoW.jpeg', bbox_inches='tight')
+plt.show()
 
 
+######
+### Emission content - Histogramme FRANCE
+######
+### Emissions content by sector
+inc_emis_cont_fr= inc_emis_cont.loc[inc_emis_cont['region']=='FR']
+
+plt.figure(figsize=(18, 12))
+sns.barplot(x="sector", y="emission content", data=inc_emis_cont_fr,palette='deep')
+plt.xlabel("Sector code", size=12)
+plt.ylabel("g$\mathrm{CO}_2$eq/\euro", size=12)
+plt.title("Total emission content - France", size=12)
+plt.savefig(OUTPUTS_PATH+'fig_emis_cont_FR_tot.jpeg', bbox_inches='tight')
+plt.show()
+plt.close()
+
+######
+### Decomposition - Emission content FRANCE
+######
+inc_emis_cont_decomp_fr= np.transpose(inc_emis_cont_decomp.loc[('FR')])
+
+#sns.set()
+inc_emis_cont_decomp_fr.T.plot(kind='bar', stacked=True, figsize=(18, 12))
+plt.xlabel("Sector code", size=12)
+plt.xticks(rotation=0,fontsize=12)
+plt.ylabel("g$\mathrm{CO}_2$eq/\euro", size=12)
+plt.title("Total emission content decomposition- France", size=12)
+plt.savefig(OUTPUTS_PATH+'fig_emis_cont_decomp_FR_tot.jpeg', bbox_inches='tight')
+plt.show()
+plt.close()
+
+######
+### Decomposition enabled emissions by country (in gCO2/euro)
+######
+emis_enable = (io_orig.GHG_emissions.N*1e-3).sum(level=0,axis=1)
+emis_enable = emis_enable.add_prefix('Total enabled emission of ')
+emis_enable.reset_index(level=1, inplace=True)
+emis_enable = emis_enable.rename_axis(index=None)
+
+for r in region_list:
+ 
+    emis_enable_r=  emis_enable[['sector','Total enabled emission of '+str(r)]]
+    emis_enable_r = np.transpose(np.transpose(emis_enable_r).add_prefix('emis cont from '))
+    emis_enable_r['region']=emis_enable_r.index
+    emis_enable_r= emis_enable_r.pivot(index='sector', columns='region')
+    emis_enable_r = emis_enable_r.droplevel(level=0,axis=1)
+    emis_enable_r= np.transpose(emis_enable_r)
+
+    emis_enable_r.T.plot(kind='bar', stacked=True, figsize=(18, 12))
+    plt.xlabel("Sector code", size=12)
+    plt.xticks(rotation=0,fontsize=12)
+    plt.ylabel("g$\mathrm{CO}_2eq$/\euro", size=12)
+    plt.title("Total enabled emission decomposition - "+r, size=12)
+    plt.savefig(OUTPUTS_PATH+'fig_emis_cont_enabled_'+r+'_tot.jpeg', bbox_inches='tight')
+    plt.show()
+    plt.close()
 
 
+##########################
+###### TABLES to save
+##########################
+share_direct_emis = pd.DataFrame(inc_emis_cont_decomp_fr.loc['Direct emis content'].div(inc_emis_cont_fr['emission content'].replace(0, np.nan).values)*100)
+share_direct_emis = share_direct_emis.astype(float).round(1)
+share_direct_emis.columns=['% direct emissions']
+
+share_1stdownstream_emis = pd.DataFrame(inc_emis_cont_decomp_fr.loc['FO emis content'].div(inc_emis_cont_fr['emission content'].replace(0, np.nan).values)*100)
+share_1stdownstream_emis = share_1stdownstream_emis.astype(float).round(1)
+share_1stdownstream_emis.columns=['% downstream at first level']
+
+##########################
+### to review 
+emis_enable_fr_fr = emis_enable.loc['FR'][['sector','Total enabled emission of FR']]
+emis_enable_fr_fr = emis_enable_fr_fr.set_index(emis_enable_fr_fr['sector'])
+emis_enable_fr_fr = emis_enable_fr_fr.drop('sector',axis=1)
+emis_enable_fr_fr = emis_enable_fr_fr.squeeze()
+##########################
+share_dom_emis = pd.DataFrame(emis_enable_fr_fr.div(inc_emis_cont_fr['emission content'].replace(0, np.nan).values)*100)
+share_dom_emis =share_dom_emis.astype(float).round(1)
+share_dom_emis.columns=['% domestic emissions']
+
+emis_cont_fr_to_save = inc_emis_cont_fr.copy()
+emis_cont_fr_to_save.drop('region',axis=1)
+emis_cont_fr_to_save['% direct emissions']=  share_direct_emis['% direct emissions'].values
+emis_cont_fr_to_save['% downstream at first level']=  share_1stdownstream_emis['% downstream at first level'].values
+emis_cont_fr_to_save['% domestic emissions']=  share_dom_emis['% domestic emissions'].values
+emis_cont_fr_to_save = emis_cont_fr_to_save.drop(['region'], axis=1)
+emis_cont_fr_to_save = emis_cont_fr_to_save.sort_values(by=['emission content'], ascending = False )
+
+top10_table= round(emis_cont_fr_to_save[:10],1).to_latex(index=False)
+least10_table= round(emis_cont_fr_to_save[-11:],1).to_latex(index=False)
+
+print(top10_table)
+print(least10_table)
 
 
+VA = pd.DataFrame(io_orig.V.sum(axis=1, level=0).sum(axis=1),columns=['value added'])
+VA['share of VA']= np.nan
+VA['share of VA'] = VA.div(VA.sum(axis=0, level=0), level=0)*100
+
+print('Min emission content in FR (gCO2/euro of VA):',round(inc_emis_cont_fr['emission content'].min()))
+print('Max emission content in FR (gCO2/euro of VA):',round(inc_emis_cont_fr['emission content'].max()))
+print('mean emission content weighted by VA in FR (gCO2/euro of VA):', round(np.average(inc_emis_cont_fr['emission content'], weights=VA.loc[('FR')]['share of VA'])))
+print('Standard deviation of emission content in FR (gCO2/euro of VA):',round(inc_emis_cont_fr['emission content'].std()))
+
+print('mean emission content weighted by VA in RoW (gCO2/euro of VA):', round(np.average(inc_emis_cont.loc[inc_emis_cont['region']=='RoW','emission content'], weights=VA.loc[('RoW'),'share of VA'])))
+print('Standard deviation of emission content in RoW (gCO2/euro of VA):', round(inc_emis_cont.loc[inc_emis_cont['region']=='RoW','emission content'].std()))
 
 
-
-
-
-
-
-
-
+##########################
+###### SAVED file - to link with INSEE survey 
+##########################
+ut.df_to_csv_with_comment(inc_emis_cont_fr, output_folder + os.sep + 'emission_content_france.csv', '# file automatically generated by ' + os.path.basename( os.getcwd()), sep=';')
+#inc_emis_cont.to_excel(OUTPUTS_PATH+'inc_emis_cont_pre.xlsx')
